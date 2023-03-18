582198de89d2252c31e86d4c6b75298d76e838a3
Ethan Chang <echang@lyft.com>
2023-03-18 14:04:56 -0700

    Add content script

--- synopsis-extension-dud/background.js
+++ synopsis-extension-dud/background.js
@@ -1,17 +1,3 @@
 "use strict";
 
-console.error("here");
-document.onmouseup = getText;
-function getText(e) {
-  const selection = document.getSelection ? document.getSelection() : document.selection.createRange();
-
-  if (selection.rangeCount > 0) {
-    const range = selection.getRangeAt(0);
-    const beforeText = range.startContainer.textContent.slice(0, range.startOffset);
-    const afterText = range.endContainer.textContent.slice(range.endOffset);
-    const surroundText = range.startContainer.textContent;
-
-    console.error(surroundText);
-  
-  }
-}
+console.log("here");
